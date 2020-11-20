ab35d7f8feac626e1b8df1c1855baf85fe1b8495
class02 <class02@Class02T01>
2020-11-20 16:33:34 +0900

    ...

--- AjaxCom/WebContent/WEB-INF/resources/js/member.js
+++ AjaxCom/WebContent/WEB-INF/resources/js/member.js
@@ -1,10 +1,24 @@
+
 $(function(){
-	$('.listBtn').on('click', function(){
+
+	function clearInfo(){
+		$('#tname').html('');
+		$('#no').html('');
+		$('#mname').html('');
+		$('#mid').html('');
+		$('#mmail').html('');
+		$('#jdate').html('');
+	}
+
+	$('.listBtn').click(function(){
 		var sno = $(this).attr('id');
+/*		
+		// 방법1
 		$('#mno').val(sno);
-		
-		$('#infobox').stop().slideUp(300);
-		
+//		var formData = new FormData($('#frm')[0]);
+*/	
+		/*
+		// 방법 2
 		var el = $(document.createElement('form'));
 		$(el).attr('method', 'POST');
 		$(el).attr('encType', 'multipart/form-data');
@@ -14,41 +28,38 @@ $(function(){
 		$(subEl).val(sno);
 		$(el).append(subEl);
 		
-		//var formData = new FormData($(el)[0]);
-		
-		formData = new FormData($('#frm')[0]);
-		function clearInfo(){
-			$('#tname').html('');
-			$('#no').html('');
-			$('#mname').html('');
-			$('#mid').html('');
-			$('#mmail').html('');
-			$('#jdate').html('');
-		}
-		
-		$.ajax({
-			url: '/euns/member/memberDetail.euns',
-			type: 'POST',
-			processData: false,
-			contentType: false,
-			data: formData,
-			success: function(obj){
-				obj = JSON.parse(obj);
-				clearInfo();
-				$('#avt').attr('src', '/euns/img/avatar/' + obj.avatar);
-				$('#tname').html(obj.name);
-				$('#no').html(obj.mno);
-				$('#mname').html(obj.name);
-				$('#mid').html(obj.id);
-				$('#mmail').html(obj.mail);
-				$('#gen').html((obj.gen = 'M')? '남자' : '여자');
-				$('#jdate').html(obj.sdate);
-				
-				$('#infobox').stop().slideDown(300);
-			},
-			error: function(){
-				alert('### 통신에러 ###');
-			}
+		var formData = new FormData($(el)[0]);
+		*/
+		// 방법 3
+		var formData = new FormData();
+		formData.set('mno', sno);
+
+		$('#infobox').slideUp(200, function(){
+			$.ajax({
+				url: '/euns/member/memberDetail.euns',
+				type: 'POST',
+				dataType: 'json',
+				async: false,
+				processData: false,
+				contentType: false,
+				data: formData,
+				success: function(obj){
+					clearInfo();
+					$('#avt').attr('src', '/euns/img/avatar/' + obj.avatar);
+					$('#tname').html(obj.name);
+					$('#no').html(obj.mno);
+					$('#mname').html(obj.name);
+					$('#mid').html(obj.id);
+					$('#mmail').html(obj.mail);
+					$('#gen').html((obj.gen = 'M')? '남자' : '여자');
+					$('#jdate').html(obj.sdate);
+					
+					$('#infobox').stop().slideDown(300);
+				},
+				error: function(){
+					alert('### 통신에러 ###');
+				}
+			});
 		});
 	});
 });
